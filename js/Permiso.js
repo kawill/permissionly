adcb99c864ec4752cb0e35a33de9766b4360696b
Nicole Cano <arbolkiri@gmail.com>
2015-03-03 14:04:03 -0600

    router is working

--- js/Permiso.js
+++ js/Permiso.js
@@ -1,7 +1,53 @@
- var App = React.createClass({
-    render:function(){
-      return <h1>Hello World</h1>
-    }
-  });
+;
+(function(exports) {
+    "use strict";
 
-  React.render(<App />, document.body);
+    Backbone.PermisoRouter = Backbone.Router.extend({
+
+        initialize: function() {
+            console.log("initialized");
+
+            this.model = new Backbone.HomeModel();
+            this.view = new Backbone.HomeView({
+                model: this.model
+            });
+            Backbone.history.start();
+        },
+
+        routes: {
+            "*default": "home",
+        },
+        home: function() {
+            this.view.render();
+            // this.view2.render(); //Temporary: we'll move the detail view later
+        }
+
+    })
+
+    Backbone.HomeModel = Backbone.Model.extend({
+        defaults: {
+
+        }
+    })
+
+    Backbone.HomeView = Backbone.TemplateView.extend({
+        el: ".container",
+        view: "Testtemp",
+        events: {
+            "click": "signUp",
+            "click ": "signIn"
+        },
+        signUp: function(event) {
+            event.preventDefault();
+            console.log(signUp);
+        },
+        signIn: function(event) {
+            event.preventDefault();
+            console.log(signIn);
+        }
+    })
+
+
+
+
+})(typeof module === "object" ? module.exports : window)
